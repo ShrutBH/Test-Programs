255f38e7d313446fb1c882c0bc5ba86650fd82c3
ShruBH <ishrutihiregoudar@gmail.com>
2025-03-02 15:38:50 +0530

    Changed the program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,16 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-// addition program
-        int a=10,b=20;
-        int c=a+b;
-        int d=a*b;
-        int e=a-b;
-        System.out.println("Addition of two numbers is "+c);
-        System.out.println(d);
-        System.out.println(e);
+Scanner scanner=new Scanner(System.in);
+System.out.println("Enter first number");
+int a =scanner.nextInt();
+System.out.println("Enter second number");
+int b=scanner.nextInt();
+int largest=a>b?a:b;
+System.out.println("The largest number is "+largest);
+
     }
 }
